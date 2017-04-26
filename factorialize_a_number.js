4cd21fd580d0c21d908ce078e010dfe3dd24184d
Donnie <dcanimation@gmail.com>
2017-04-26 13:57:33 -0500

    Update factorialize_a_number.js

--- factorialize_a_number.js
+++ factorialize_a_number.js
@@ -20,17 +20,11 @@ Starting Point
  */
  
 function factorialize(num) {
-  var numArr = [];
-  var newNum = 1;
-  for (var i = 1; i < num + 1; i++) {
-    numArr.push(i);
-  }
-  for (var j = 0; j < numArr.length; j++) {
-    newNum = newNum * numArr[j];
-    console.log(newNum);
+  var newNum = 1;                         //create a starting point for our 'storage' variable
+  for (var i = 1; i < num + 1; i++) {     //loop through the number to get all of the factors
+    newNum = newNum * i;                  //multiply each subsequent factor by the next in the list
   }
   num = newNum;
-  console.log(num);
   return num;
 }
 
