7e6f2db77adf5eee115cb05705ca865964a0af1b
Donnie <dcanimation@gmail.com>
2017-04-28 16:19:30 -0500

    Create find_the_longest_word.js

---
+++ find_the_longest_word.js
@@ -0,0 +1,15 @@
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+//START
+
+function findLongestWord(str) {
+  return str.length;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+ */
+ 
+ 
