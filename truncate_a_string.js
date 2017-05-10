148e27108b51f6fed71020bde37d4fade75d2e54
Donnie <dcanimation@gmail.com>
2017-05-10 12:04:29 -0500

    Update truncate_a_string.js

--- truncate_a_string.js
+++ truncate_a_string.js
@@ -16,3 +16,18 @@ does not add to the string length in determining the truncated string.
   truncateString("A-tisket a-tasket A green and yellow basket", 11);
 */
 
+//The wording on this problem made it tricky to determin how to set up the if statement...
+function truncateString(str, num) {
+  if (str.length > num) {
+    if (num <= 3) {
+      str = str.slice(0, num) + "..."; 
+    } else {
+      str = str.slice(0, (num - 3)) + "...";
+    }
+  }
+   
+  console.log(str);
+  return str;
+}
+
+truncateString("A-", 1);
