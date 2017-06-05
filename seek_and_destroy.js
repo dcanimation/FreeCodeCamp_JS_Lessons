891a74d7b024bdac3cb95db597229f30b8a4b081
Donnie <dcanimation@gmail.com>
2017-06-05 14:21:33 -0500

    Update seek_and_destroy.js

--- seek_and_destroy.js
+++ seek_and_destroy.js
@@ -11,3 +11,21 @@ Remove all elements from the initial array that are of the same value as these a
   destroyer([1, 2, 3, 1, 2, 3], 2, 3);
 
 */
+
+function destroyer(arr) {
+  var args = Array.from(arguments);
+  var newArr = [];
+  function destroy(value) {
+    return value != args[i]; 
+  }
+  
+  for( var i = 0; i < args.length; i++){
+    if (i > 0){
+      arr = arr.filter(destroy);
+    }
+    console.log(arr);
+  }
+  return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
